5d1a496c5b5dfaa095d69a48a1e6466acb71cbc0
amirreza <rajabiamir@gmail.com>
2022-02-06 14:51:10 +0330

    creating new calculator

---
+++ calculator.py
@@ -0,0 +1,34 @@
+print("</>creating for Amirreza</>")
+a = int(input('Add1:'))
+b = int(input('Add2:'))
+
+
+print('...................')
+
+
+print('hasel zarb:')
+print(a * b)
+
+
+print('...................')
+
+
+print('hasel taghsim:')
+print(a/b)
+
+
+print('...................')
+
+
+print('hasel jameh:')
+print(a+b)
+
+
+print('...................')
+
+
+print('hasel tafregh:')
+print(a-b)
+
+print('...................')
+print("finish")
